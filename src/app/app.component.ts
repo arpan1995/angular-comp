2faaac3bc319e68d6ad773ceee7b98b8edd8743b
arpan1995 <thearpandas47@gmail.com>
2022-07-21 11:55:11 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Arpan';
+  public name = 'Arpan';
+  public myId = 'testID';
+  public successClass = "text-success";
+  public haserror = true
+  public titlestyle = {color: 'red', fontStyle: 'italic'}
+  public greet = "";
+  public timeN = new Date;
+
+  public isDisable = true;
+  greetUser(){
+    return this.name;
+  }
+  ngOninit(){
+
+  }
+  timenow(){
+    console.log(this.timeN)
+  }
+
+  onClick(){
+    console.log("hello")
+    this.greet = "Arpan Das"
+  }
 }
